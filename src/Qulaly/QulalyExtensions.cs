c6eaf648c63c1a3b0835e3ad55a2948dcfac7976
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-08-16 23:23:12 +0900

    Add extension method for SyntaxTree

--- src/Qulaly/QulalyExtensions.cs
+++ src/Qulaly/QulalyExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Qulaly.Matcher;
@@ -7,10 +7,19 @@ namespace Qulaly
 {
     public static class QulalyExtensions
     {
+        public static IEnumerable<SyntaxNode> QuerySelectorAll(this SyntaxTree syntaxTree, string selector, Compilation? compilation = default)
+        {
+            return QuerySelectorAll(syntaxTree.GetRoot(), selector, compilation);
+        }
+
         public static IEnumerable<SyntaxNode> QuerySelectorAll(this SyntaxNode node, string selector, Compilation? compilation = default)
         {
-            var parsedSelector = QulalySelector.Parse(selector);
-            return EnumerableMatcher.GetEnumerable(node, parsedSelector, compilation?.GetSemanticModel(node.SyntaxTree));
+            return QuerySelectorAll(node, QulalySelector.Parse(selector), compilation);
+        }
+
+        public static IEnumerable<SyntaxNode> QuerySelectorAll(this SyntaxTree syntaxTree, QulalySelector selector, Compilation? compilation = default)
+        {
+            return QuerySelectorAll(syntaxTree.GetRoot(), selector, compilation);
         }
 
         public static IEnumerable<SyntaxNode> QuerySelectorAll(this SyntaxNode node, QulalySelector selector, Compilation? compilation = default)
@@ -18,10 +27,19 @@ namespace Qulaly
             return EnumerableMatcher.GetEnumerable(node, selector, compilation?.GetSemanticModel(node.SyntaxTree));
         }
 
+        public static SyntaxNode? QuerySelector(this SyntaxTree syntaxTree, string selector, Compilation? compilation = default)
+        {
+            return QuerySelector(syntaxTree.GetRoot(), selector, compilation);
+        }
+
         public static SyntaxNode? QuerySelector(this SyntaxNode node, string selector, Compilation? compilation = default)
         {
-            var parsedSelector = QulalySelector.Parse(selector);
-            return EnumerableMatcher.GetEnumerable(node, parsedSelector, compilation?.GetSemanticModel(node.SyntaxTree)).FirstOrDefault();
+            return QuerySelector(node, QulalySelector.Parse(selector), compilation);
+        }
+
+        public static SyntaxNode? QuerySelector(this SyntaxTree syntaxTree, QulalySelector selector, Compilation? compilation = default)
+        {
+            return QuerySelector(syntaxTree.GetRoot(), selector, compilation);
         }
 
         public static SyntaxNode? QuerySelector(this SyntaxNode node, QulalySelector selector, Compilation? compilation = default)
